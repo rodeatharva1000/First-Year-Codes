df1f1740068cbc51417618339c11fec2fb91eec1
rodeatharva1000 <123895899+rodeatharva1000@users.noreply.github.com>
2023-03-14 09:41:41 +0530

    Create Investment and loan.py

---
+++ Investment and loan.py
@@ -0,0 +1,20 @@
+inp1 = int(input("Enter the loan or Investment amount:"))
+inp2 = int(input("Enter number of years : "))
+inp3 = int(input("Enter intrest on loan : "))
+def calculate() :
+    if inp3==0 :
+        print("Constant amount ! ")
+    else :
+    	global inp1
+    	amount_add = inp1/inp3
+    	inp1 = inp1 + amount_add
+    	return amount_add
+i = 1
+for years in range(0,inp2+1) :
+    calculate()
+    print(f"for {i} year --> {inp1}")
+    i = i+1
+	
+	
+	
+
