170d5a33fa18666284e0af7999aac8358d5ce4e8
rodeatharva1000 <123895899+rodeatharva1000@users.noreply.github.com>
2023-03-20 21:00:02 +0530

    Update Investment and loan.py

--- Investment and loan.py
+++ Investment and loan.py
@@ -1,12 +1,12 @@
 inp1 = int(input("Enter the loan or Investment amount:"))
 inp2 = int(input("Enter number of years : "))
-inp3 = int(input("Enter intrest on loan : "))
+inp3 = int(input("Enter intrest or vaj on loan : "))
 def calculate() :
     if inp3==0 :
         print("Constant amount ! ")
     else :
     	global inp1
-    	amount_add = inp1/inp3
+    	amount_add = (inp1/100)*inp3
     	inp1 = inp1 + amount_add
     	return amount_add
 i = 1
